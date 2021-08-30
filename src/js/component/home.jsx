17a721806e64dfde579c1ae66ac7a40efb7e5cce
priscila torres reyes <priscyla.torres16@hotmail.com>
2021-08-30 19:54:41 +0000

    traffic

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,38 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
+import "./style.css";
 
 //create your first component
 const Home = () => {
+	const [bombillaACtiva, setBombillasActivas] = useState();
+	const bottonA = () => {
+		setBombillasActivas("a");
+	};
+	const bottonB = () => {
+		setBombillasActivas("b");
+	};
+	const bottonC = () => {
+		setBombillasActivas("c");
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="bg-dark d-inline-flex flex-column p-2 rounded">
+				<div
+					onClick={bottonA}
+					className={`bg-danger rounded-circle p-3 ${
+						bombillaACtiva == "a" ? "active" : " "
+					}`}></div>
+				<div
+					onClick={bottonB}
+					className={`bg-warning rounded-circle p-3 ${
+						bombillaACtiva == "b" ? "active" : " "
+					}`}></div>
+				<div
+					onClick={bottonC}
+					className={`bg-success rounded-circle p-3 ${
+						bombillaACtiva == "c" ? "active" : " "
+					}`}></div>
+			</div>
 		</div>
 	);
 };
